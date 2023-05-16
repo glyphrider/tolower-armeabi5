e1f795197d18ef2c028ebcaff078aeb944c3533f
Brian H. Ward <glyphrider@gmail.com>
2023-05-16 19:12:36 +0100

    local labels (where appropriate)

--- tolower.s
+++ tolower.s
@@ -11,34 +11,35 @@ ldr r1, =buffer
 mov r2, #buflen @ buffer length (one byte)
 swi 0
 
-mov r3,r0
-cmp r3, #0
-beq _exit
+mov r3,r0  @ remember how many characters we read
+cmp r3, #0 @ if we read zero bytes (we're done)
+beq _exit  @ then branch to exit
 
-@ confirm the character is within range
+@ walk the buffer and lower any uppercase characters
 ldr r1,=buffer
 mov r2,#0 @ start at index zero
-_loop:
+1:
 ldrb r0,[r1,r2] @ just read one byte (because the buffer will be packed with chars)
+@ confirm the character is within range
 cmp r0,#'A'
-blt skip
+blt 2f
 cmp r0,#'Z'
-bgt skip
+bgt 2f
 @ shift the character down
 @ flip the lowercase bit....
 @ faster than subtracting and adding (or even just adding the offset)
 eor r0,#0x20
 strb r0,[r1,r2] @ just write one byte so we keep the char array righteous
-skip:
+2:
 add r2,r2,#1 @ increment index
-cmp r2,r3 @ check against upper bound
-blt _loop @ loop if not there yet
+cmp r2,r3 @ check against upper bound (number of characters read)
+blt 1b @ loop if not there yet
 
 @ write one character to STDOUT from the buffer
 mov r7, #4 @ __NR_write
 mov r0, #1 @ STDOUT_FILENO
 ldr r1, =buffer
-mov r2, r3 @ buffer length (from read)
+mov r2, r3 @ buffer length (number of characters read)
 swi 0
 
 b _start
